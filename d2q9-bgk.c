315ffdcf084efbd46bb83392258210e672e85f7e
an4 <ad12461@my.bristol.ac.uk>
2014-11-19 19:09:02 +0000

    fix

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -321,10 +321,11 @@ int accelerate_flow(const t_param params, t_speed* cells, int* obstacles, int in
         }
     }
 
+    int size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     if(index != 0 && size > 1) {
-        int rank, size, up, down;
+        int rank, up, down;
         MPI_Status status;  
 
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
